7271beb8ea5e85fd328cb4d34e3ae07d1e561e97
mirainokatachi <contact@miraino-katachi.co.jp>
2019-07-16 13:24:00 +0900

    first commit3

---
+++ action.php
@@ -0,0 +1,24 @@
+﻿<?php
+    require_once($_SERVER['DOCUMENT_ROOT'].'/todo2/TodoItemsModel.php');
+
+    
+try {
+
+    $db = new TodoItemsModel();
+    
+    foreach ($_POST['is_completed'] as $key => $value) {
+        $db->completeTodoItem($key,$value);
+    }
+
+    foreach ($_POST['is_deleted'] as $key => $value) {
+        $db->deleteTodoItem($key,$value);
+    }
+
+    $dbh=null;
+    header("Location: index.php");
+    
+    }
+    catch (Exception $e) {
+        print 'ただいま障害により大変ご迷惑をお掛けしております。'.$e;
+        exit();
+    }
